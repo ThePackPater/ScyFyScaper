78d21b1a4f02082b3873dcdf0a28b2f26f71ba18
Andy Pack <thepackpater@gmail.com>
2019-05-23 15:19:00 -0500

    deleted controllers coded article and note JS in models

---
+++ models/article.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Articleschema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	link: {
+		type: String,
+		required: true,
+	},
+	summary: {
+		type: String,
+		default: "Summary unavailable."
+	},
+	img: {
+		type: String,
+		// default: "/assets/images/unavailable.jpg"
+	},
+	issaved: {
+		type: Boolean,
+		default: false
+	},
+	status: {
+		type: String,
+		default: "Save Article"
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	note: {
+		type: Schema.Types.ObjectId,
+		ref: "Note"
+	}
+});
+
+Articleschema.index({title: "text"});
+
+const Article  = mongoose.model("Article", Articleschema);
+module.exports = Article;
